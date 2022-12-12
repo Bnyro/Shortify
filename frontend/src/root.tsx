ed09b3707b1de9455ed16810671e46ec0165846c
Bnyro <bnyro@tutanota.com>
2022-12-12 15:17:15 +0100

    add a favicon

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -7,6 +7,7 @@ import {
   FileRoutes,
   Head,
   Html,
+  Link,
   Meta,
   Routes,
   Scripts,
@@ -21,6 +22,7 @@ export default function Root() {
         <Title>SolidStart - Bare</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Link rel="icon" href="/favicon.svg" />
       </Head>
       <Body>
         <Suspense>
